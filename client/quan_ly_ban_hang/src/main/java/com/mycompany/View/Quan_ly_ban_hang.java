412f9db000b7fdf0f74bc62ac1f7151e066d1cb0
mihvu <minhvu.utt@gmail.com>
2024-10-02 01:30:16 +0700

    #4 - Update View Quan Ly Ban Hang

--- client/quan_ly_ban_hang/src/main/java/com/mycompany/View/Quan_ly_ban_hang.java
+++ client/quan_ly_ban_hang/src/main/java/com/mycompany/View/Quan_ly_ban_hang.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.quan_ly_ban_hang;
+package com.mycompany.View;
 
 /**
  *
